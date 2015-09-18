0c73dff1f617e8b6f68c05e53dd6dfceed07aacc
Jiri Popek <Jiri.Popek@bmw.de>
2015-09-18 13:50:11 +0200

    Button class defined, code cleanup and reuse

--- MIDIcontroller.ino
+++ MIDIcontroller.ino
@@ -9,77 +9,72 @@
 // https://github.com/thomasfredericks/Bounce-Arduino-Wiring
 
 #include <Bounce2.h>
-// 1st row
-#define BUTTON_1x1 2
-#define BUTTON_1x2 3
-#define BUTTON_1x3 4
-#define BUTTON_1x4 5
-// 2nd row
-#define BUTTON_2x1 6
-#define BUTTON_2x2 7
-#define BUTTON_2x3 8
-// 3rd row
-#define BUTTON_3x1 9
-#define BUTTON_3x2 10
 
 #define LED_PIN 13
-#define MIDI_CHANNEL 1
-#define INTERVAL 10
+#define DEFAULT_MIDI_CHANNEL 1
+#define BOUNCE_INTERVAL 10  // [ms]
 
 MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-// Instantiate a Bounce object
-Bounce debouncer1x1 = Bounce();
-Bounce debouncer1x2 = Bounce();
-Bounce debouncer1x3 = Bounce();
-Bounce debouncer1x4 = Bounce();
+// the controller board is 4x3 buttons
+#define BOARD_ROWS 4
+#define BOARD_COLS 3
 
-Bounce debouncer2x1 = Bounce();
-Bounce debouncer2x2 = Bounce();
-Bounce debouncer2x3 = Bounce();
+Bounce *buttons[BOARD_ROWS * BOARD_COLS] = {NULL,};
 
-Bounce debouncer3x1 = Bounce();
-Bounce debouncer3x2 = Bounce();
+class Button : public Bounce
+{
+
+public:
+
+  // constructor
+  Button(uint8_t pin, uint8_t cc, uint8_t value=0, uint8_t channel=DEFAULT_MIDI_CHANNEL)
+                                    : m_cc(cc)
+                                    , m_value(value)
+                                    , m_channel(DEFAULT_MIDI_CHANNEL)
+
+  {
+    Bounce::pin = pin;
+    pinMode(pin, INPUT_PULLUP);
+    attach(pin);
+    interval(BOUNCE_INTERVAL);
+  }
+
+  bool update() {
+    Bounce::update();
+    if (fell()) {
+      MIDI.sendControlChange(m_cc, m_value, m_channel);
+    }
+  }
+
+private:
+  uint8_t m_interval;
+  uint8_t m_cc;
+  uint8_t m_value;
+  uint8_t m_channel;
+};
+
+
+
 
 void setup() {
 
-  // Setup the button with an internal pull-up :
-  pinMode(BUTTON_1x1, INPUT_PULLUP);
-  pinMode(BUTTON_1x2, INPUT_PULLUP);
-  pinMode(BUTTON_1x3, INPUT_PULLUP);
-  pinMode(BUTTON_1x4, INPUT_PULLUP);
-
-  pinMode(BUTTON_2x1, INPUT_PULLUP);
-  pinMode(BUTTON_2x2, INPUT_PULLUP);
-  pinMode(BUTTON_2x3, INPUT_PULLUP);
-
-  pinMode(BUTTON_3x1, INPUT_PULLUP);
-  pinMode(BUTTON_3x2, INPUT_PULLUP);
-
-  // After setting up the button, setup the Bounce instance :
-  debouncer1x1.attach(BUTTON_1x1);
-  debouncer1x2.attach(BUTTON_1x2);
-  debouncer1x3.attach(BUTTON_1x3);
-  debouncer1x4.attach(BUTTON_1x4);
-
-  debouncer2x1.attach(BUTTON_2x1);
-  debouncer2x2.attach(BUTTON_2x2);
-  debouncer2x3.attach(BUTTON_2x3);
-
-  debouncer3x1.attach(BUTTON_3x1);
-  debouncer3x2.attach(BUTTON_3x2);
-
-  debouncer1x1.interval(INTERVAL); // interval in ms
-  debouncer1x2.interval(INTERVAL);
-  debouncer1x3.interval(INTERVAL);
-  debouncer1x4.interval(INTERVAL);
-
-  debouncer2x1.interval(INTERVAL);
-  debouncer2x2.interval(INTERVAL);
-  debouncer2x3.interval(INTERVAL);
-
-  debouncer3x1.interval(INTERVAL);
-  debouncer3x2.interval(INTERVAL);
+  // add buttons to the array and nstantiate the button objects
+  // first row (bottom)
+  buttons[0] = new Button(/* pin */2, /* cc */1);  // PRESET A
+  buttons[1] = new Button(/* pin */3, /* cc */2);  // PRESET B
+  buttons[2] = new Button(/* pin */4, /* cc */3);  // PRESET C
+  buttons[3] = new Button(/* pin */5, /* cc */4);  // PRESET D
+  // second row
+  buttons[4] = new Button(/* pin */6, /* cc */21);  // STOMP 1
+  buttons[5] = new Button(/* pin */7, /* cc */22);  // STOMP 2
+  buttons[6] = new Button(/* pin */8, /* cc */23);  // STOMP 3
+  buttons[7] = NULL;
+  // third row (top)
+  buttons[8] = new Button(/* pin */9, /* cc */31);  // TUNER
+  buttons[9] = new Button(/* pin */10, /* cc */32); // BYPASS
+  buttons[10] = NULL;
+  buttons[11] = NULL;
 
   //Setup the LED
   pinMode(LED_PIN, OUTPUT);
@@ -88,68 +83,10 @@ void setup() {
 }
 
 void loop() {
-/*
-JamUp
-4  Wah depth
-7 Master volume
-10  Previous preset
-11  Next preset
-15  Toggle metronome
-20  Toggle selected effect
-21  Toggle noisegate effect
-22  Toggle stomp effect
-23  Toggle filter effect
-24  Toggle modulation effect
-25  Toggle delay effect
-26  Toggle reverb effect
-27  Toggle amplifier
-30  Toggle jam player
-31  Jam player increase speed
-32  Jam player decrease speed
-33  Jam player increase pitch
-34  Jam player decrease pitch
-35  Jam player increase volume
-36  Jam player decrease volume
-40  Sampler toggle record
-41  Sampler undo
-42  Sampler toggle play
-43  Sampler toggle dub
-64  Tap tempo
-  */
   // Update the Bounce instance
-  debouncer1x1.update();
-  debouncer1x2.update();
-  debouncer1x3.update();
-  debouncer1x4.update();
-
-  debouncer2x1.update();
-  debouncer2x2.update();
-  debouncer2x3.update();
-
-  debouncer3x1.update();
-  debouncer3x2.update();
-
-  if (debouncer1x1.fell()) {  // PRESET A
-    MIDI.sendControlChange(1, 0, MIDI_CHANNEL);
-  }
-  if (debouncer1x2.fell()) {  // PRESET B
-    MIDI.sendControlChange(2, 0, MIDI_CHANNEL);
-  }
-  if (debouncer1x3.fell()) {  // PRESET C
-    MIDI.sendControlChange(3, 0, MIDI_CHANNEL);
-  }
-  if (debouncer1x4.fell()) {  // PRESET C
-    MIDI.sendControlChange(4, 0, MIDI_CHANNEL);
-  }
-
-
-  if (debouncer2x1.fell()) { // STOMP
-    MIDI.sendControlChange(22, 0, MIDI_CHANNEL);
-  }
-  if (debouncer2x2.fell()) {  // DELAY
-    MIDI.sendControlChange(25, 0, MIDI_CHANNEL);
-  }
-  if (debouncer2x3.fell()) {  // MOD
-    MIDI.sendControlChange(24, 0, MIDI_CHANNEL);
+  for (int i=0; i<BOARD_ROWS*BOARD_COLS; i++) {
+    if (buttons[i] != NULL) {
+      buttons[i]->update();
+    }
   }
 }
